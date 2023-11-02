315ecab6660ebd8c8351776fa5e6c796e2327dc3
Martin Tomassi <martin.tomassi@studio.unibo.it>
2023-11-02 21:26:01 +0100

    Exercise Number 64 Completed

--- 64-design-with-maps/src/main/java/it/unibo/collections/social/TestSocialNetworkUser.java
+++ 64-design-with-maps/src/main/java/it/unibo/collections/social/TestSocialNetworkUser.java
@@ -48,11 +48,11 @@ public final class TestSocialNetworkUser {
          * 
          * * Adam Smith, asmith, (no age)
          */
-        final SocialNetworkUser<User> kbacon = null; //TODO
-        final SocialNetworkUser<User> dwashington = null; //TODO
-        final SocialNetworkUser<User> mgladwell = null; //TODO
-        final SocialNetworkUser<User> ntaleb = null; //TODO
-        final User asmith = null; //TODO
+        final SocialNetworkUser<User> kbacon = new SocialNetworkUserImpl<User>("Kevin", "Bacon", "kbacon", 56);
+        final SocialNetworkUser<User> dwashington = new SocialNetworkUserImpl<User>("Denzel", "Washington", "dwashington", 59);
+        final SocialNetworkUser<User> mgladwell = new SocialNetworkUserImpl<User>("Malcom", "Gladwell", "mgladwell", 51);
+        final SocialNetworkUser<User> ntaleb = new SocialNetworkUserImpl<User>("Nicholas", "Taleb", "ntaleb", 54);
+        final User asmith = new UserImpl("Adam", "Smith", "asmith");
         /*
          * Make people follow each other
          */
--- 64-design-with-maps/src/main/java/it/unibo/collections/social/impl/SocialNetworkUserImpl.java
+++ 64-design-with-maps/src/main/java/it/unibo/collections/social/impl/SocialNetworkUserImpl.java
@@ -8,12 +8,11 @@ import it.unibo.collections.social.api.User;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.HashSet;
 
 /**
  * 
@@ -36,7 +35,8 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      * In order to save the people followed by a user organized in groups, adopt
      * a generic-type Map:  think of what type of keys and values would best suit the requirements
      */
-
+    private final static int DEFAULT_AGE = -1;
+    private final Map<String,Set<U>> followedUser; 
     /*
      * [CONSTRUCTORS]
      *
@@ -48,6 +48,12 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      * - username
      * - age and every other necessary field
      */
+    public SocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge, 
+            final Map<String,Set<U>> followedUser) {
+        super(name, surname, user, userAge);
+        this.followedUser = followedUser;
+    }
+
     /**
      * Builds a user participating in a social network.
      *
@@ -62,21 +68,35 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      *            application
      */
     public SocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
-        super(null, null, null, 0);
+        super(name, surname, user, userAge);
+        this.followedUser = new HashMap<String,Set<U>>();
     }
 
     /*
      * 2) Define a further constructor where the age defaults to -1
      */
+    public SocialNetworkUserImpl(String name, String surname, String user, String firstName, String lastName, String username) {
+        super(name, surname, user, DEFAULT_AGE);
+        this.followedUser = new HashMap<String,Set<U>>();
+    }
 
     /*
      * [METHODS]
      *
      * Implements the methods below
      */
-    @Override
     public boolean addFollowedUser(final String circle, final U user) {
-        return false;
+        if (this.followedUser.containsKey(circle)) {
+                this.followedUser.containsKey(circle);
+                Set<U> setLinkedToValue = this.followedUser.get(circle);
+                setLinkedToValue.add(user);
+                this.followedUser.put(circle, setLinkedToValue);
+        } else {
+            Set<U> set = new HashSet<U>();
+            this.followedUser.put(circle, set);
+            set.add(user);
+        }
+        return true;
     }
 
     /**
@@ -84,13 +104,24 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      * [NOTE] If no group with groupName exists yet, this implementation must
      * return an empty Collection.
      */
-    @Override
     public Collection<U> getFollowedUsersInGroup(final String groupName) {
-        return null;
+        for (final String i : this.followedUser.keySet()) {
+            if (i.equals(groupName)) {
+                return new HashSet<U>(this.followedUser.get(groupName));
+            }
+        }
+        return new HashSet<U>();
     }
 
-    @Override
     public List<U> getFollowedUsers() {
-        return null;
+        List<U> list = new ArrayList<>();
+        for (final Set<U> setUser : this.followedUser.values()) {
+            list.addAll(setUser);
+        }
+        return list;
+    }
+
+    public boolean add(User e) {
+        return false;
     }
 }
